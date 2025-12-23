78057058ebc78b3b6e259b74b360ab2d33ba3eda
jonatan ocampo morales <jonaocampo16@gmail.com>
2025-12-22 21:15:17 -0500

    primer commit

--- app/router/chatBot.py
+++ app/router/chatBot.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from app.bot.procesador import responder, analizar_texto
 from app.schemas.chatBot import ChatMessage
+from fastapi import Request
+
+from twilio.twiml.messaging_response import MessagingResponse
+
 router = APIRouter()
 
 @router.post("/chat")
@@ -13,4 +17,17 @@ def chat(data: ChatMessage):
 def analizar(data: ChatMessage):
     info = analizar_texto(data.mensaje)
     return info
+
+@router.post("/whatsapp")
+async def whatsapp_webhook(request: Request):
+    form_data = await request.form()
+    mensaje_usuario = form_data.get("Body")
+
+    respuesta_bot = responder(mensaje_usuario)
+
+    twilio_resp = MessagingResponse()
+
+    twilio_resp.message(respuesta_bot)
+
+    return Response(content=str(twilio_resp), media_type="application/xml")
 
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from app.router import chatBot
+
+
 app = FastAPI()
 
 
 app.include_router(chatBot.router, prefix="/chat", tags=["chat"])
+
+
+
+
 
 @app.get("/")
 async def root():
